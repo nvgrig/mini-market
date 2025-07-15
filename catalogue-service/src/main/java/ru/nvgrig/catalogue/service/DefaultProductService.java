1d0b6f2fa5cad72fde088860865541f04b4f0ed6
n.grigoriev <nikita_grigoriev@senla.eu>
2025-07-15 22:12:46 +0400

    adding filter form

--- catalogue-service/src/main/java/ru/nvgrig/catalogue/service/DefaultProductService.java
+++ catalogue-service/src/main/java/ru/nvgrig/catalogue/service/DefaultProductService.java
@@ -17,7 +17,7 @@ public class DefaultProductService implements ProductService {
     @Override
     public Iterable<Product> findAllProducts(String filter) {
         if (filter != null && !filter.isBlank()) {
-            return productRepository.findAllByTitleLikeIgnoreCase(filter);
+            return productRepository.findAllByTitleLikeIgnoreCase("%" + filter + "%");
         } else {
             return productRepository.findAll();
         }
--- manager-app/src/main/java/ru/nvgrig/manager/controller/ProductsController.java
+++ manager-app/src/main/java/ru/nvgrig/manager/controller/ProductsController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.nvgrig.manager.client.BadRequestException;
 import ru.nvgrig.manager.client.ProductsRestClient;
 import ru.nvgrig.manager.controller.payload.NewProductPayload;
@@ -18,8 +19,9 @@ public class ProductsController {
     private final ProductsRestClient productsRestClient;
 
     @GetMapping(value = "list")
-    public String getProductsList(Model model) {
-        model.addAttribute("products", productsRestClient.findAllProducts());
+    public String getProductsList(Model model, @RequestParam(name = "filter", required = false) String filter) {
+        model.addAttribute("products", productsRestClient.findAllProducts(filter));
+        model.addAttribute("filter", filter);
         return "catalogue/products/list";
     }
 
