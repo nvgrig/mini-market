795b72a55b25b57eac3864ed6445df04f20bb082
n.grigoriev <nikita_grigoriev@senla.eu>
2025-08-12 11:23:55 +0400

    adding basic auth

--- manager-app/src/main/java/ru/nvgrig/manager/config/ClientBeans.java
+++ manager-app/src/main/java/ru/nvgrig/manager/config/ClientBeans.java
@@ -3,6 +3,7 @@ package ru.nvgrig.manager.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
 import ru.nvgrig.manager.client.RestClientProductsRestClient;
 
@@ -10,9 +11,12 @@ import ru.nvgrig.manager.client.RestClientProductsRestClient;
 public class ClientBeans {
     @Bean
     public RestClientProductsRestClient restClientProductsRestClient(
-            @Value("${market.service.catalogue.uri:http://localhost:8081}") String catalogueBaseUrl) {
+            @Value("${market.service.catalogue.uri:http://localhost:8081}") String catalogueBaseUrl,
+            @Value("${market.service.catalogue.username:}") String catalogueUserName,
+            @Value("${market.service.catalogue.password:}") String cataloguePassword) {
         return new RestClientProductsRestClient(RestClient.builder()
                 .baseUrl(catalogueBaseUrl)
+                .requestInterceptor(new BasicAuthenticationInterceptor(catalogueUserName, cataloguePassword))
                 .build());
     }
 }
