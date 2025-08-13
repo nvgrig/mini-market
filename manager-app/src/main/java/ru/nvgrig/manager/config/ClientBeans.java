7142cad5a718189703e109b2adf849cfa60a3fa1
n.grigoriev <nikita_grigoriev@senla.eu>
2025-08-13 09:26:53 +0400

    adding keycloak auth

--- manager-app/src/main/java/ru/nvgrig/manager/config/ClientBeans.java
+++ manager-app/src/main/java/ru/nvgrig/manager/config/ClientBeans.java
@@ -3,20 +3,26 @@ package ru.nvgrig.manager.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestClient;
 import ru.nvgrig.manager.client.RestClientProductsRestClient;
+import ru.nvgrig.manager.security.OAuthClientHttpRequestInterceptor;
 
 @Configuration
 public class ClientBeans {
     @Bean
     public RestClientProductsRestClient restClientProductsRestClient(
             @Value("${market.service.catalogue.uri:http://localhost:8081}") String catalogueBaseUrl,
-            @Value("${market.service.catalogue.username:}") String catalogueUserName,
-            @Value("${market.service.catalogue.password:}") String cataloguePassword) {
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository,
+            @Value("${market.service.catalogue.registration-id:keycloak}") String registrationId) {
         return new RestClientProductsRestClient(RestClient.builder()
                 .baseUrl(catalogueBaseUrl)
-                .requestInterceptor(new BasicAuthenticationInterceptor(catalogueUserName, cataloguePassword))
+                .requestInterceptor(new OAuthClientHttpRequestInterceptor(
+                        new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientRepository),
+                        registrationId))
                 .build());
     }
 }
