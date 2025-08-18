acf901aedb8f79edd9900c6ffadcbbc00c83e07f
n.grigoriev <nikita_grigoriev@senla.eu>
2025-08-18 23:29:38 +0400

    wiremock integration test

--- manager-app/src/test/java/ru/nvgrig/manager/config/TestingBeans.java
+++ manager-app/src/test/java/ru/nvgrig/manager/config/TestingBeans.java
@@ -1,9 +1,13 @@
 package ru.nvgrig.manager.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.web.client.RestClient;
+import ru.nvgrig.manager.client.RestClientProductsRestClient;
 
 import static org.mockito.Mockito.mock;
 
@@ -18,5 +22,15 @@ public class TestingBeans {
     @Bean
     public OAuth2AuthorizedClientRepository authorizedClientRepository() {
         return mock(OAuth2AuthorizedClientRepository.class);
+    }
+
+    @Bean
+    @Primary
+    public RestClientProductsRestClient testRestClientProductsRestClient(
+            @Value("${market.service.catalogue.uri:http://localhost:54321}") String catalogueBaseUri
+    ) {
+        return new RestClientProductsRestClient(RestClient.builder()
+                .baseUrl(catalogueBaseUri)
+                .build());
     }
 }
